245e604b54b57e01c28874ca34c2949a7788ed92
Ethan Wessel <ejwessel@gmail.com>
2020-09-25 19:48:19 -0700

    safe transfer

--- contracts/TokenAirdrop.sol
+++ contracts/TokenAirdrop.sol
@@ -1,9 +1,13 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.6.10;
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 
 contract TokenAirdrop is Ownable {
+    using SafeERC20 for IERC20;
+
     function distributeTokens(
         IERC20 token,
         address[] memory users,
@@ -11,7 +15,7 @@ contract TokenAirdrop is Ownable {
     ) external onlyOwner {
         require(users.length == amounts.length, "LENGTH_MISMATCH");
         for (uint256 i = 0; i < users.length; i++) {
-            token.transferFrom(msg.sender, users[i], amounts[i]);
+            token.safeTransferFrom(msg.sender, users[i], amounts[i]);
         }
     }
 }
--- test-integration/TokenBatchDistributor.js
+++ test-integration/TokenBatchDistributor.js
@@ -4,17 +4,55 @@ const {
   expectEvent, // Assertions for emitted events
   expectRevert, // Assertions for transactions that should fail
 } = require("@openzeppelin/test-helpers");
+const { BN } = require("@openzeppelin/test-helpers");
 const IERC20 = artifacts.require("IERC20");
+const ERC20 = artifacts.require("ERC20");
 const TokenAirdrop = artifacts.require("TokenAirdrop");
+
+const DAI_WHALE = '0x131a9A36Ea25aFB4Ed1a4510eE4B36E369d0F699'
+const USDC_WHALE = '0x8cee3eeab46774c1CDe4F6368E3ae68BcCd760Bf'
+const USDT_WHALE = '0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8'
+const FWB_WHALE = '0xBA9FEc0023e6AA54D96617cDb3E5507FF20F8B81'
+
+async function formattedAmount(token, value) {
+  const decimals = await token.decimals.call()
+  return ((new BN("10").pow(decimals)).mul(new BN(value))).toString()
+}
+
+async function acquireToken(fundAccount, receiver, token, amount) {
+  // NOTE: Ganache is setup to control the WHALE addresses. This method moves requeted funds out of the fund account and into the specified wallet
+
+  // fund the account with ETH so it can move funds
+  await web3.eth.sendTransaction({ from: receiver, to: fundAccount, value: 1e10 })
+
+  const funds = await formattedAmount(token, amount)
+
+  await token.transfer(receiver, funds, { from: fundAccount })
+  const tokenBal = await token.balanceOf(receiver)
+  console.log(`${token.address} Balance: ${tokenBal.toString()}`)
+}
 
 contract("TokenAirdrop Unit Test", async (accounts) => {
   const [owner, randomUser1, randomUser2, randomUser3] = accounts;
 
   let distributor
   let FWB
+  let DAI
+  let USDC
+  let USDT
 
   before(async () => {
-    FWB = await IERC20.at("0x7d91e637589EC3Bb54D8213a9e92Dc6E8D12da91")
+
+    DAI = await ERC20.at("0x6B175474E89094C44Da98b954EedeAC495271d0F")
+    USDC = await ERC20.at("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
+    USDT = await ERC20.at("0xdAC17F958D2ee523a2206206994597C13D831ec7")
+    FWB = await ERC20.at("0x7d91e637589EC3Bb54D8213a9e92Dc6E8D12da91")
+
+    await acquireToken(DAI_WHALE, owner, DAI, "1000")
+    await acquireToken(USDC_WHALE, owner, USDC, "1000")
+    await acquireToken(USDT_WHALE, owner, USDT, "1000")
+    await acquireToken(FWB_WHALE, owner, FWB, "1000")
+
     distributor = await TokenAirdrop.new()
     console.log(distributor.address)
   });
@@ -55,6 +93,75 @@ contract("TokenAirdrop Unit Test", async (accounts) => {
       console.log(user2Bal.toString())
       assert.equal(user2Bal.toNumber(), 10)
       const user3Bal = await FWB.balanceOf(randomUser3)
+      console.log(user3Bal.toString())
+      assert.equal(user3Bal.toNumber(), 10)
+    })
+
+    it("Test distributeTokens distributes to users DAI", async () => {
+      await DAI.approve(distributor.address, 30)
+      await distributor.distributeTokens(
+        DAI.address,
+        [randomUser1, randomUser2, randomUser3],
+        [10, 10, 10],
+        { from: owner }
+      )
+
+      const ownerBal = await DAI.balanceOf(owner)
+      console.log(ownerBal.toString())
+
+      const user1Bal = await DAI.balanceOf(randomUser1)
+      console.log(user1Bal.toString())
+      assert.equal(user1Bal.toNumber(), 10)
+      const user2Bal = await DAI.balanceOf(randomUser2)
+      console.log(user2Bal.toString())
+      assert.equal(user2Bal.toNumber(), 10)
+      const user3Bal = await DAI.balanceOf(randomUser3)
+      console.log(user3Bal.toString())
+      assert.equal(user3Bal.toNumber(), 10)
+    })
+
+    it("Test distributeTokens distributes to users USDC", async () => {
+      await USDC.approve(distributor.address, 30)
+      await distributor.distributeTokens(
+        USDC.address,
+        [randomUser1, randomUser2, randomUser3],
+        [10, 10, 10],
+        { from: owner }
+      )
+
+      const ownerBal = await USDC.balanceOf(owner)
+      console.log(ownerBal.toString())
+
+      const user1Bal = await USDC.balanceOf(randomUser1)
+      console.log(user1Bal.toString())
+      assert.equal(user1Bal.toNumber(), 10)
+      const user2Bal = await USDC.balanceOf(randomUser2)
+      console.log(user2Bal.toString())
+      assert.equal(user2Bal.toNumber(), 10)
+      const user3Bal = await USDC.balanceOf(randomUser3)
+      console.log(user3Bal.toString())
+      assert.equal(user3Bal.toNumber(), 10)
+    })
+
+    it("Test distributeTokens distributes to users USDT", async () => {
+      await USDT.approve(distributor.address, 30)
+      await distributor.distributeTokens(
+        USDT.address,
+        [randomUser1, randomUser2, randomUser3],
+        [10, 10, 10],
+        { from: owner }
+      )
+
+      const ownerBal = await USDT.balanceOf(owner)
+      console.log(ownerBal.toString())
+
+      const user1Bal = await USDT.balanceOf(randomUser1)
+      console.log(user1Bal.toString())
+      assert.equal(user1Bal.toNumber(), 10)
+      const user2Bal = await USDT.balanceOf(randomUser2)
+      console.log(user2Bal.toString())
+      assert.equal(user2Bal.toNumber(), 10)
+      const user3Bal = await USDT.balanceOf(randomUser3)
       console.log(user3Bal.toString())
       assert.equal(user3Bal.toNumber(), 10)
     })
