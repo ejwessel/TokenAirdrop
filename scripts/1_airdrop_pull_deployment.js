f3616ad2625d8d3bb12d1033a17a3a2424f0ffd2
Ethan Wessel <ejwessel@gmail.com>
2020-09-30 20:25:37 -0700

    generate signatures for users to claim

--- scripts/1_airdrop_pull_deployment.js
+++ scripts/1_airdrop_pull_deployment.js
@@ -28,12 +28,22 @@ async function main() {
   console.log(`FWB Address: ${FWB.address}`)
   console.log(`Distributor Address: ${distributor.address}`)
 
-  // move funds into the contract
+  // ensure to move funds into the contract
   const balance = amount(decimals, 1000)
-  await FWB.transfer(distributor.address, balance)
+  // await FWB.transfer(distributor.address, balance)
 
   // create all signatures for users and amounts
-  const signature = await generateSignature(process.env.PRIVATE_KEY, FWB.address, amount(decimals, 1000), user1)
+  user_and_amounts = {
+    '0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0': { amount: amount(decimals, 10), signature: '' },
+    '0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b': { amount: amount(decimals, 10), signature: '' },
+    '0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d': { amount: amount(decimals, 10), signature: '' },
+  }
+
+  for (const [key, value] of Object.entries(user_and_amounts)) {
+    value.signature = await generateSignature(process.env.PRIVATE_KEY, FWB.address, value.amount, key)
+  }
+
+  console.log(user_and_amounts)
 
   // NOTE:  users now go claim with provided signature similar to the following
   // await distributor.claim(FWB.address, user1, amt, signature);
